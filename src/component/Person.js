5de5bf9c6b5a2df1c242ae62a940b661fdfb4725
thamerblg <thamerbelgacem18@yahoo.fr>
2021-09-25 18:49:30 +0200

    file modified

--- src/component/Person.js
+++ src/component/Person.js
@@ -29,7 +29,7 @@ class Person extends Component {
 
   componentWillUnmount = () => {
     //console.log("run componentWillUnmount ");
-    clearInterval(this.state.timer);
+    clearInterval(this.state.counter);
   };
 
   render() {
